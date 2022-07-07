494993fb9fbac1c3ec0aa1c2a63f181972920323
ngaruiya <mosengaruiya704@gmail.com>
2022-07-07 11:59:07 +0300

    MYPORTFOLIO

---
+++ public/style.css
@@ -0,0 +1,50 @@
+body{
+    font-family: 'poppin', 'kimoda';
+    margin: 0;
+ }
+ section{
+    background-color: antiquewhite;
+ }
+ .hero{
+    background-color: aqua;
+    height: 100vh;
+ }
+ nav{
+    display: flex;
+    justify-content: space-between;
+ }
+ ul{
+    display: flex;
+    list-style-type: none;
+    width: 500px;
+    justify-content:  space-around;
+    font-size: 30px;
+ }
+ li, a{
+    text-decoration: none;
+ }
+ .h1{
+    font-size: 90px;
+    margin: 0;
+ }
+ .hero,area{
+    display: flex;
+    height: 100%;
+    justify-content: space-between;
+    align-items: center;
+ }
+ .hero,text{
+    margin-left: 500px;
+    margin-bottom: 30px;
+ }
+ .button{
+    width: 200px;
+    height: 60px;
+    border-radius: 100px;
+    background-color: blueviolet;
+    text-align: center;
+    font-size: 30px;
+    font-weight: 800;
+    padding-top: 10px;
+    margin-top: 100px;
+}
